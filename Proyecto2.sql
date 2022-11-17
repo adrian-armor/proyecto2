d9382952204ceb1820cc7253351f941b28c525d1
Adrián Araya-Morera <ctec.adrian@gmail.com>
2022-11-16 19:22:03 -0600

    Obtiene creditos
    Se agrega funcion que obtiene los créditos de un curso

--- Proyecto2.sql
+++ Proyecto2.sql
@@ -1,152 +1,145 @@
 set serveroutput on;
 
-create table provincia
-(id_provincia number not null,
-nombre varchar2(20) not null,
-constraint id_provincia_pk
-primary key(id_provincia)
+create table provincia (
+    id_provincia number not null,
+    nombre varchar2(20) not null,
+    constraint id_provincia_pk
+    primary key(id_provincia)
 );
 
-create table estudiante
-(carnet number not null,
-nombre varchar2(20) not null,
-provincia number not null,
-constraint fk_provincia
-foreign key (provincia) references provincia (id_provincia),
-constraint carnet_pk
-primary key(carnet)
+create table estudiante (
+    carnet number not null,
+    nombre varchar2(20) not null,
+    provincia number not null,
+    constraint fk_provincia
+    foreign key (provincia) references provincia (id_provincia),
+    constraint carnet_pk
+    primary key(carnet)
 );
 
-create table beca
-(id_beca number not null,
-monto float,
-nombre_beca varchar2(20),
-constraint id_beca_pk
-primary key(id_beca)
+create table beca (
+    id_beca number not null,
+    monto float,
+    nombre_beca varchar2(20),
+    constraint id_beca_pk
+    primary key(id_beca)
 );
 
-
-create table aula
-(id_aula number not null,
-nombre_aula varchar2(20) not null,
-constraint id_aula_pk
-primary key(id_aula)
+create table aula (
+    id_aula number not null,
+    nombre_aula varchar2(20) not null,
+    constraint id_aula_pk
+    primary key(id_aula)
 );
 
-create table horario
-(id_horario number not null,
-dia varchar2(20),
-hora_inicio varchar2(20),
-hora_fin varchar2(20),
-constraint id_horario_pk
-primary key(id_horario)
+create table horario (
+    id_horario number not null,
+    dia varchar2(20),
+    hora_inicio varchar2(20),
+    hora_fin varchar2(20),
+    constraint id_horario_pk
+    primary key(id_horario)
 );
 
-create table materia
-(id_materia number not null,
-creditos number(1),
-nombre_materia varchar2(50) not null,
-constraint id_materia_pk
-primary key(id_materia)
+create table materia (
+    id_materia number not null,
+    creditos number(1),
+    nombre_materia varchar2(50) not null,
+    constraint id_materia_pk
+    primary key(id_materia)
 );
 
-create table profesor
-(id_profesor number not null,
-nombre_profesor varchar2(20) not null,
-constraint id_profesor_pk
-primary key(id_profesor)
+create table profesor (
+    id_profesor number not null,
+    nombre_profesor varchar2(20) not null,
+    constraint id_profesor_pk
+    primary key(id_profesor)
 );
 
-
-
-create table departamento
-(id_depto number not null,
-nombre_depto varchar2(20) not null,
-constraint id_depto_pk
-primary key(id_depto)
+create table departamento (
+    id_depto number not null,
+    nombre_depto varchar2(20) not null,
+    constraint id_depto_pk
+    primary key(id_depto)
 );
 
-
-create table grupo
-(num_grupo number not null,
-id_profesor_grupo number not null,
-id_horario_grupo number not null,
-id_aula_grupo number not null,
-id_materia_grupo number not null,
-constraint fk_id_profesor_grupo
-foreign key(id_profesor_grupo) references profesor(id_profesor),
-constraint fk_id_horario_grupo
-foreign key(id_horario_grupo) references horario(id_horario),
-constraint fk_id_aula_grupo
-foreign key(id_aula_grupo) references aula(id_aula),
-constraint fk_id_materia_grupo
-foreign key(id_materia_grupo) references materia(id_materia),
-constraint num_grupo_pk
-primary key(num_grupo)
+create table grupo (
+    num_grupo number not null,
+    id_profesor_grupo number not null,
+    id_horario_grupo number not null,
+    id_aula_grupo number not null,
+    id_materia_grupo number not null,
+    constraint fk_id_profesor_grupo
+    foreign key(id_profesor_grupo) references profesor(id_profesor),
+    constraint fk_id_horario_grupo
+    foreign key(id_horario_grupo) references horario(id_horario),
+    constraint fk_id_aula_grupo
+    foreign key(id_aula_grupo) references aula(id_aula),
+    constraint fk_id_materia_grupo
+    foreign key(id_materia_grupo) references materia(id_materia),
+    constraint num_grupo_pk
+    primary key(num_grupo)
 );
 
-
-create table matricula
-(id_matricula number not null,
-carnet_matricula number not null,
-id_grupo_matriculado number not null,
-nota_estudiante number(3,2) not null,
-constraint fk_carnet_matricula
-foreign key(carnet_matricula) references estudiante(carnet),
-constraint fk_id_grupo_matriculado
-foreign key(id_grupo_matriculado) references grupo(num_grupo),
-primary key (id_matricula, carnet_matricula, id_grupo_matriculado)
+create table matricula (
+    id_matricula number not null,
+    carnet_matricula number not null,
+    id_grupo_matriculado number not null,
+    nota_estudiante number(3,2) not null,
+    constraint fk_carnet_matricula
+    foreign key(carnet_matricula) references estudiante(carnet),
+    constraint fk_id_grupo_matriculado
+    foreign key(id_grupo_matriculado) references grupo(num_grupo),
+    primary key (id_matricula, carnet_matricula, id_grupo_matriculado)
 );
 
-create table periodo
-(periodo number(1),
-carnet_estudiante number not null,
-id_grupo_matriculado number not null,
-año varchar2(4),
-nota_final number(3),
-estado varchar2(20),
-constraint fk_carnet_estudiante_periodo
-foreign key(carnet_estudiante) references estudiante(carnet),
-constraint fk_id_grupo_matriculado_periodo
-foreign key(id_grupo_matriculado) references grupo(num_grupo),
-primary key (periodo, carnet_estudiante, id_grupo_matriculado)
+create table periodo (
+    periodo number(1),
+    carnet_estudiante number not null,
+    id_grupo_matriculado number not null,
+    año varchar2(4),
+    nota_final number(3),
+    estado varchar2(20),
+    constraint fk_carnet_estudiante_periodo
+    foreign key(carnet_estudiante) references estudiante(carnet),
+    constraint fk_id_grupo_matriculado_periodo
+    foreign key(id_grupo_matriculado) references grupo(num_grupo),
+    primary key (periodo, carnet_estudiante, id_grupo_matriculado)
 );
 
 --N:N
-create table estudiante_beca
-(carnet_eb number not null,
-id_beca_eb number not null,
-constraint fk_carnet_eb
-foreign key (carnet_eb) references estudiante(carnet),
-constraint fk_id_beca_eb
-foreign key (id_beca_eb) references beca(id_beca),
-primary key(carnet_eb, id_beca_eb)
+create table estudiante_beca (
+    carnet_eb number not null,
+    id_beca_eb number not null,
+    constraint fk_carnet_eb
+    foreign key (carnet_eb) references estudiante(carnet),
+    constraint fk_id_beca_eb
+    foreign key (id_beca_eb) references beca(id_beca),
+    primary key(carnet_eb, id_beca_eb)
 );
 
 --N:1
 
 alter table profesor
- add (id_departamento_profesor number not null)
- add constraint fk_id_departamento_profesor
- foreign key(id_departamento_profesor) references departamento(id_depto);
+    add (id_departamento_profesor number not null)
+    add constraint fk_id_departamento_profesor
+    foreign key(id_departamento_profesor) references departamento(id_depto);
 
 alter table materia
- add(id_departamento_materia number not null)
- add constraint fk_id_departamento_materia
- foreign key(id_departamento_materia) references departamento(id_depto);
+    add(id_departamento_materia number not null)
+    add constraint fk_id_departamento_materia
+    foreign key(id_departamento_materia) references departamento(id_depto);
 
-
-
-create table requisito_materia
-(id_materia number not null,
-id_materia_requisito number not null,
-constraint fk_id_materia
-foreign key(id_materia) references materia(id_materia),
-constraint fk_id_materia_requisito
-foreign key(id_materia_requisito) references materia(id_materia),
-primary key(id_materia, id_materia_requisito)
+create table requisito_materia (
+    id_materia number not null,
+    id_materia_requisito number not null,
+    constraint fk_id_materia
+    foreign key(id_materia) references materia(id_materia),
+    constraint fk_id_materia_requisito
+    foreign key(id_materia_requisito) references materia(id_materia),
+    primary key(id_materia, id_materia_requisito)
 );
-
+/*
 -- datos de prueba
 insert into provincia values(1, 'Guanacaste');
 insert into provincia values(2, 'San Jose');
@@ -256,11 +249,11 @@ insert into requisito_materia values(8,7);
 insert into requisito_materia values(9,4);
 insert into requisito_materia values(9,10);
 insert into requisito_materia values(10,1);
-
+*/
 
 --2)
 create or replace procedure matricula_estudiante(
-        p_carnet_estudiante in number, 
+        p_carnet_estudiante in number,
         p_id_grupo in number
         )
         as
@@ -269,23 +262,23 @@ create or replace procedure matricula_estudiante(
         v_max_id_matricular number;
         -- cursor al c_id_materia_requisito de las materias que conincidan con el p_carnet_estudiante en la tabla perido
         cursor c_id_materia_requisito is
-                select id_materia from requisito_materia re 
-                        where re.id_materia_requisito = (select id_materia from grupo where num_grupo = p_id_grupo) and 
-                            (select id_materia from grupo where num_grupo = p_id_grupo) in ( 
-                                select id_materia_requisito from requisito_materia req 
+                select id_materia from requisito_materia re
+                        where re.id_materia_requisito = (select id_materia from grupo where num_grupo = p_id_grupo) and
+                            (select id_materia from grupo where num_grupo = p_id_grupo) in (
+                                select id_materia_requisito from requisito_materia req
                                     where req.id_materia in (
-                                        select id_materia_grupo from grupo gr 
+                                        select id_materia_grupo from grupo gr
                                             where gr.num_grupo in (
-                                                select id_grupo_matriculado from periodo per 
+                                                select id_grupo_matriculado from periodo per
                                                     where per.carnet_estudiante = p_carnet_estudiante and per.estado = 'Aprobado')));
-                begin 
+                begin
                 select nombre into v_nombre_estudiante from estudiante where carnet = p_carnet_estudiante;
                 OPEN c_id_materia_requisito;
                 LOOP
                     FETCH c_id_materia_requisito INTO v_id_materia;
-                        IF c_id_materia_requisito%FOUND THEN 
-                            dbms_output.put_line('El estudiante ' || v_nombre_estudiante || 
-                                                 ' no puede matricular el grupo ' || p_id_grupo || 
+                        IF c_id_materia_requisito%FOUND THEN
+                            dbms_output.put_line('El estudiante ' || v_nombre_estudiante ||
+                                                 ' no puede matricular el grupo ' || p_id_grupo ||
                                                  ' porque no cumple con los requisitos necesarios');;
                             exit;
                         ELSE
@@ -295,9 +288,8 @@ create or replace procedure matricula_estudiante(
                             exit;
                         END IF;
                 END LOOP;
-                CLOSE c_id_materia_requisito; 
+                CLOSE c_id_materia_requisito;
                 end;
-
 
 
 -- 3
@@ -311,4 +303,68 @@ créditos.
         SUM(Nota X Creditos) / Total de créditos = Promedio ponderado
 
 Crear una función que calcule el promedio ponderado (basado en el último período matriculado) para un estudiante dado. Dicha función debe ser invocada por un procedimiento que reciba como parámetro un año e inserte en una tabla temporal la información de todos estudiantes que tengan carné con ese año (carnet, nombre, ponderado). Dicha tabla temporal se consultará después de la ejecución del procedimiento.
+*/
+-- 3
+/*
+El promedio ponderado se utiliza para asignar la cita de matrícula, este se calcula de la siguiente forma:
+• Se multiplica la cantidad de créditos de cada curso por la nota obtenida.
+• Se suman todos los resultados del punto anterior y se divide entre la cantidad total de créditos.
+
+        SUM(Nota X Creditos) / Total de créditos = Promedio ponderado
+
+Crear una función que calcule el promedio ponderado (basado en el último período matriculado) para un estudiante dado. Dicha función debe ser invocada por un procedimiento que reciba como parámetro un año e inserte en una tabla temporal la información de todos estudiantes que tengan carné con ese año (carnet, nombre, ponderado). Dicha tabla temporal se consultará después de la ejecución del procedimiento.
+
+CREATE OR REPLACE FUNCTION calcula_ponderado_indv (carnet IN NUMBER)
+    RETURN NUMBER IS
+    sumatoria NUMBER;
+    total_creditos NUMBER;
+    ponderado NUMBER;
+
+    v_periodo NUMBER;
+    v_id_grupo NUMBER;
+    v_nota_final NUMBER;
+
+    v_id_materia NUMBER; -- se usará para extraer el id de la materia y los créditos
+    --SELECT id_materia_grupo from grupo WHERE num_grupo = 8 INTO v_id_materia;
+    --SELECT creditos from materia WHERE id_materia = v_id_materia;
+
+    CURSOR periodos_estd IS
+        SELECT periodo, id_grupo_matriculado, nota_final FROM periodo WHERE carnet_estudiante = carnet ORDER BY año DESC, periodo DESC;
+    BEGIN
+        OPEN periodos_estd;
+        LOOP
+            FETCH periodos_estd
+            INTO v_periodo, v_id_grupo, v_nota_final,
+*/
+
+CREATE OR REPLACE FUNCTION obtiene_creditos (n_grupo IN NUMBER)
+    RETURN NUMBER IS
+    v_id_materia NUMBER(1); -- se usará para extraer el id de la materia y los créditos
+    v_creditos NUMBER(1);
+    BEGIN
+    SELECT id_materia_grupo INTO v_id_materia from grupo WHERE num_grupo = n_grupo;
+    SELECT creditos INTO v_creditos from materia WHERE id_materia = v_id_materia;
+    RETURN v_creditos;
+END;
+/
+
+-- Prueba de ejecución
+SET SERVEROUTPUT ON;
+DECLARE
+    creditos NUMBER(1);
+BEGIN
+    creditos:=obtiene_creditos(9); --Análisis de Algoritmos 4 creditos
+    DBMS_OUTPUT.PUT_LINE ('Los créditos del curso solicitado son: ' || creditos);
+END;
+/
+
+/*
+DROP TABLE profesor CASCADE CONSTRAINTS;
+DROP TABLE departamento CASCADE CONSTRAINTS;
+DROP TABLE grupo CASCADE CONSTRAINTS;
+DROP TABLE matricula CASCADE CONSTRAINTS;
+DROP TABLE periodo CASCADE CONSTRAINTS;
+DROP TABLE estudiante_beca CASCADE CONSTRAINTS;
+DROP TABLE materia CASCADE CONSTRAINTS;
+DROP TABLE requisito_materia CASCADE CONSTRAINTS;
 */
